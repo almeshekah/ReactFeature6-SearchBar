0b2dab58325198e70acbac57e30d8fa79183de29
Abdullah ALmeshekah <meshekah@MacBook-Pro.local>
2021-01-24 15:13:37 +0300

    not done

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,18 +2,28 @@
 import { ListWrapper } from "../styles";
 // Components
 import ProductItem from "./ProductItem";
+import { useState  } from "react";
 import SearchBar from "./SearchBar";
+
+
 // Data
 import products from "../products";
 
+
 const ProductList = () => {
+  const [input, setInput] = useState('');
   const productList = products.map((product) => (
     <ProductItem product={product} key={product.id} />
   ));
+  const filtered = products.filter(product => {
+    return product.name.toLowerCase().includes(input.toLowerCase())
+   })
 
   return (
     <>
-      <SearchBar />
+      <SearchBar 
+      input={input} 
+      onChange={ setInput  }/>
       <ListWrapper>{productList}</ListWrapper>
     </>
   );
